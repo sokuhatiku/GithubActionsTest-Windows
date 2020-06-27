98d7292eba56372299606297aac3db21b9c464cc
Sokuhatiku <sokuhatiku@gmail.com>
2020-06-28 02:00:49 +0900

    ライセンス情報を使ってビルドテストを行うよう変更

---
+++ Assets/Editor/CI/Builder.cs
@@ -0,0 +1,12 @@
+﻿using UnityEditor;
+
+namespace CI
+{
+    public static class Builder 
+    {
+        public static void Build()
+        {
+            AssetDatabase.ExportPackage("Assets/Scenes", "Build.unitypackage");
+        }
+    }
+}
